d3bd004e872e3df63d4b8d6b03fd2537341a9db9
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-06-01 20:34:56 +0300

    Task 1 completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,3 +1,16 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let myValue = document.querySelector("#search").value;
+  document.getElementById("output").innerText =
+    "It is: \n" +
+    myValue * 2.2046 +
+    " pounds \n" +
+    myValue * 1000 +
+    " grams or \n" +
+    myValue * 35.274 +
+    " ounces";
+});
+
 /* ------------------------------ TASK 1 ----------------------------
 Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
 pamatyti jo pateikto svorio kovertavimą į:
