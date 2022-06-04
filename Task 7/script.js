089516e17a3f8c25f21a1a8399368a2512fdeb04
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-06-04 13:59:42 +0300

    Task 7 completed

--- Task 7/script.js
+++ Task 7/script.js
@@ -1,12 +1,20 @@
+const audi = {
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+
+function showObjectKeys(x) {
+  let objKeys = Object.keys(x);
+  console.log(objKeys);
+}
+
+showObjectKeys(audi);
+
 /* ------------------------------ TASK 7 -----------------------------------
 Turimas "audi" objektas.
 
 Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
-const audi = {
-  make: 'audi',
-  model: 'A6',
-  year: 2005,
-  color: 'white',
-};
