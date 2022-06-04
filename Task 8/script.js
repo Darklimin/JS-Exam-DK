fac858a9b2e0322659c38ae1c845f67d7863646f
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-06-04 14:22:44 +0300

    Task 8 completed

--- Task 8/script.js
+++ Task 8/script.js
@@ -1,3 +1,26 @@
+function Calculator(a, b) {
+  this.a = a;
+  this.b = b;
+  this.sum = function () {
+    console.log(a + b);
+  };
+  this.subtraction = function () {
+    console.log(a - b);
+  };
+  this.multiplication = function () {
+    console.log(a * b);
+  };
+  this.division = function () {
+    console.log(a / b);
+  };
+}
+
+const example = new Calculator(15, 5);
+example.sum();
+example.subtraction();
+example.multiplication();
+example.division();
+
 /* ------------------------------ TASK 8 --------------------------------------------
 Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri gebės sukurti objektus su 4 metodais:
 sum(a, b) - priima du skaičius ir grąžina jų sumą.
